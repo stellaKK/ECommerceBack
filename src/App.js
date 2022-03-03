c9f6ac1e3f30438797666671581d9df729ed575f
stellaKK <35387152+stellaKK@users.noreply.github.com>
2022-03-03 17:08:44 -0500

    update router url

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ function App() {
         <div className="App">
 
           <Routes>
-            <Route path={`/login`} element={<Login />} />
+            <Route path="/ecommerceback/login" element={<Login />} />
 
-            <Route path={`/inventory/*`} element={<InventoryMain />} />
-            <Route path={`/invoices/*`} element={<OrderMain />} />
-            <Route path={`/prices/*`} element={<PriceMain />} />
-            <Route path={`/clients/*`} element={<ClientPageMain />} />
-            <Route path={`/messages/*`} element={<MessageMain />} />
-            <Route path={`/tasks/*`} element={<TaskMain />} />
+            <Route path="/ecommerceback/inventory/*" element={<InventoryMain />} />
+            <Route path="/ecommerceback/invoices/*" element={<OrderMain />} />
+            <Route path="/ecommerceback/prices/*" element={<PriceMain />} />
+            <Route path="/ecommerceback/clients/*" element={<ClientPageMain />} />
+            <Route path="/ecommerceback/messages/*" element={<MessageMain />} />
+            <Route path="/ecommerceback/tasks/*" element={<TaskMain />} />
 
-            <Route path={`/`} element={<Dashboard />} />
+            <Route path="/ecommerceback/" element={<Dashboard />} />
           </Routes>
         </div>
       </Router>
